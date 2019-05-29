579373055e9d5112aa5881b8773522fa7d14108d
Noriya Saeki <noriya.saeki@gmail.com>
2019-05-29 12:02:26 +0900

    chore: rails gコマンドでCSS,JS,Helper作成抑制

--- config/application.rb
+++ config/application.rb
@@ -25,9 +25,9 @@ module InstaClone
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
